c7f1c129f2cd0a9d756c6ca4cf0baa99f4e7dd2b
Twinklekumari2 <twinklekumari246@gmail.com>
2025-12-05 18:31:50 +0530

    hero section added

--- frontend/emergency/src/pages/HomePage.jsx
+++ frontend/emergency/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-
+import HeroSection from '../components/HeroSection'
+import './../styles/pages.css'
 const HomePage = () => {
   return (
-    <div>
+    <div className='home-page'>
         <Navbar/>
+        <HeroSection/>
     </div>
   )
 }
