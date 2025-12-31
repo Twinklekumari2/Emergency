60172839b4b509029ca440fc294fdc5001f9a801
Twinklekumari2 <twinklekumari246@gmail.com>
2025-12-31 22:19:53 +0530

    api file updated

--- frontend/emergency/src/api.js
+++ frontend/emergency/src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://emergency-y32u.onrender.com",
-  baseURL:"http://localhost:4000/",
+  baseURL: "https://emergency-y32u.onrender.com",
+  // baseURL:"http://localhost:4000/",
   withCredentials: true, // 🔥 important
   headers: {
     "Content-Type": "application/json"
